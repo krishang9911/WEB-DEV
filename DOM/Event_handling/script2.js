b66969b2ac51c71a1a267343e112bfbb5a0b90ae
Krishang <krishangmahajan9911@gmail.com>
2026-03-19 23:36:10 +0530

    Added script2.js and index.js in Event_handling folder

---
+++ DOM/Event_handling/script2.js
@@ -0,0 +1,38 @@
+// Mouse events
+//  Those browser events that occur when the user interacts with
+//   with the mouse - clicking, hovering, moving or pressing
+//    mouse buttons on elements in a webpage
+
+//  Common mouse events
+//    mousedown
+//    mouseup
+//    click
+//    dblclick
+//    mouseover
+//    mousemove
+//    mouseout
+//    mouseleave
+//    mouseenter
+//    wheel
+
+//  1) mousedown
+//    fires when user presse a mouse button down
+//     on an element in a webpage
+//      Fires for ALL MOUSE BUTTONS
+//         left, right, middle
+
+//  2) mouseup
+//    fires when user releases a mouse button
+//     on an element in a webpage
+//      Fires for all mouse buttons
+//         left, right, middle
+
+// 3)  click
+//    fires when user presses the left mouse button
+//     down on an element in a webpage
+
+//  ** FIRES ONLY FOR LEFT MOUSE BUTTON **
+
+//    occurs when one mousedown and one mouseup
+//    is detected on the same element
+
