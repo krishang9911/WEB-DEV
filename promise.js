a9f7eb10c1ef834a1f0c4abaa5ec018924661a54
Krishang <krishangmahajan9911@gmail.com>
2026-03-30 23:56:13 +0530

    Added promise consumer methods

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-// A promise is a JavaScript object tht represents the eventual completion or
+// A promise is a JavaScript object that represents the eventual completion or
 // failure of an asynchronous operation and its resulting value
 
 // Three states
@@ -13,7 +13,7 @@
 
 // 3) resolved(or fulfilled)
 //    i) Represents successful completion of asynchronous operation
-//    ii) Returns 
+//    ii) Returns the result(value) of the operation
 
 let promise = new Promise(function(resolve, reject){
     let success = true;
@@ -22,5 +22,34 @@ let promise = new Promise(function(resolve, reject){
     }else{
         reject("Operation failed");
     }
-
 })
+
+// Promise consumer methods
+//  These are the methods that are used the handle the result 
+//  after a promise is completed
+
+// 1. .then()
+//    Used to handle the successful result of a promise
+//    Used when promise s resolved/fulfilled
+
+promise.then((result) => {
+    console.log(result);
+})
+
+// 2. .catch()
+//     Used when promise is rejected
+//     Receives the error
+
+promise.catch((error) => {
+    console.log(error);
+})
+
+// 3. .finally()
+//    Used to run some code regardless of success or failure
+//    Runs always, whether resolved or rejected
+
+promise.finally(() => console.log("Operation completed"))
+
+// .then()  Handles successful result
+// .catch() Handles error
+// .finally() Execution after completion
