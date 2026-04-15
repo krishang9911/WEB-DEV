ffd322a41fe83405fb176057a06828f70c285a0f
Krishang <krishangmahajan9911@gmail.com>
2026-04-15 23:43:21 +0530

    Updated closure.js

--- closure_again/script.js
+++ closure_again/script.js
@@ -21,3 +21,27 @@ innerFunction();
 // Closures Rely on Lexical Scoping which means that a function's scope is
 //  decided by where it is defined, not where it is executed. 
 //   This allows the inner function to access variables form its outer function
+
+function couter(){
+    let x = 10
+    return function(){
+        console.log(x);
+
+    }
+}
+
+outer()()
+
+//10
+
+function bank(){
+    let balance = 1000;
+return function withdraw(amount){
+    balance -= amount
+    console.log(balance}
+
+}
+
+const withdraw = bank()
+withdraw(100)
+withdraw(200)
